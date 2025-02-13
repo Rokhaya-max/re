add797b3457c2e77bebae11ac210f96bcdcc2ca9
Rokhaya-max <rokhayasignate281@gmail.com>
2025-02-13 16:03:38 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var peer; var myStream;
-function ajoutVideo(stream) { try { var video = document.createElement('video'); document.getElementById('participants').appendChild(video); video.autoplay = true; video.controls = true; video.srcObject = stream; } catch (error) { console.error(error); } }
+function ajoutVideo(stream) { 
+    try { var video = document.createElement('video'); document.getElementById('participants').appendChild(video); video.autoplay = true; video.controls = true; video.srcObject = stream; } catch (error) { console.error(error); } }
 function register() { var name = document.getElementById('name').value;
 try { peer = new Peer(name); navigator.getUserMedia({video: true, audio: true}, function(stream) { myStream = stream; ajoutVideo(stream); document.getElementById('register').style.display = 'none'; document.getElementById('userAdd').style.display = 'block'; document.getElementById('userShare').style.display = 'block'; peer.on('call', function(call) { call.answer(myStream); call.on('stream', function(remoteStream) { ajoutVideo(remoteStream); }); });
 }, function(err) { console.log('Failed to get local stream' ,err); });
